5329f18bcb55d2b9a5b1f7741d9f4ab3a461490d
ycy <app97204@gmail.com>
2022-02-27 19:32:54 +0800

    完成底部導航器功能

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,42 @@ import 'package:flutter_test_project/body.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List page = [MyBody(),MyBody2(),MyBody3(),MyBody4()];
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     var widget = MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My Flutter App 3'),
+          title: Text('Flutter Test Project'),
         ),
-        body: MyBody4(),
-        bottomNavigationBar: BottomNavigationBar(items: [
-          BottomNavigationBarItem(icon: Icon(Icons.one_k),label: 'Home1',backgroundColor: Colors.blue),
-          BottomNavigationBarItem(icon: Icon(Icons.two_k),label: 'Home2',backgroundColor: Colors.blue),
-          BottomNavigationBarItem(icon: Icon(Icons.three_k),label: 'Home3',backgroundColor: Colors.blue),
-          BottomNavigationBarItem(icon: Icon(Icons.four_k),label: 'Home4',backgroundColor: Colors.blue)
-        ]),
+        body: page[index],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+            currentIndex: index,
+            onTap: (idx){
+              setState(() {
+                index = idx;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.one_k),label: 'Home1',backgroundColor: Colors.blue),
+              BottomNavigationBarItem(icon: Icon(Icons.two_k),label: 'Home2',backgroundColor: Colors.blue),
+              BottomNavigationBarItem(icon: Icon(Icons.three_k),label: 'Home3',backgroundColor: Colors.blue),
+              BottomNavigationBarItem(icon: Icon(Icons.four_k),label: 'Home4',backgroundColor: Colors.blue)
+            ]),
       ),
     );
     return widget;
   }
 }
+
+
